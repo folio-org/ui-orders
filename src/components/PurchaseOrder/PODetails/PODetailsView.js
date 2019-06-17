eae7d97b9cab46d3f6dba819a2da4797a8e5cad8
Yury Aniskou <klaus24128@gmail.com>
2019-06-17 16:57:27 +0300

    UIOR-242 add metadata to po details view

--- src/components/PurchaseOrder/PODetails/PODetailsView.js
+++ src/components/PurchaseOrder/PODetails/PODetailsView.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 
@@ -8,6 +8,7 @@ import {
   Checkbox,
   Col,
   KeyValue,
+  MetaSection,
   Row,
 } from '@folio/stripes/components';
 
@@ -26,92 +27,107 @@ class PODetailsView extends Component {
     const addressBillTo = get(addresses.find(el => el.id === get(order, 'billTo', '')), 'address', '');
     const addressShipTo = get(addresses.find(el => el.id === get(order, 'shipTo', '')), 'address', '');
 
+    const metadata = get(order, 'metadata');
+
     return (
-      <Row>
-        <Col xs={6}>
-          <KeyValue
-            label={<FormattedMessage id="ui-orders.orderDetails.vendor" />}
-            value={get(order, 'vendorName')}
-          />
-        </Col>
-        <Col xs={6}>
-          <KeyValue
-            label={<FormattedMessage id="ui-orders.orderDetails.createdBy" />}
-            value={get(order, 'createdByName')}
-          />
-        </Col>
-        <Col xs={6}>
-          <KeyValue
-            label={<FormattedMessage id="ui-orders.orderDetails.poNumber" />}
-            value={get(order, 'poNumber')}
-          />
-        </Col>
-        <Col xs={6}>
-          <KeyValue label={<FormattedMessage id="ui-orders.orderDetails.createdOn" />}>
-            <FolioFormattedTime dateString={get(order, 'metadata.createdDate')} />
-          </KeyValue>
-        </Col>
-        <Col xs={6}>
-          <KeyValue
-            label={<FormattedMessage id="ui-orders.orderDetails.assignedTo" />}
-            value={get(order, 'assignedToUser')}
-          />
-        </Col>
-        <Col
-          className={css.addressWrapper}
-          data-test-order-details-bill-to
-          xs={6}
-        >
-          <KeyValue
-            label={<FormattedMessage id="ui-orders.orderDetails.billTo" />}
-            value={addressBillTo}
-          />
-        </Col>
-        <Col
-          className={css.addressWrapper}
-          data-test-order-details-ship-to
-          xs={6}
-        >
-          <KeyValue
-            label={<FormattedMessage id="ui-orders.orderDetails.shipTo" />}
-            value={addressShipTo}
-          />
-        </Col>
-        <Col xs={6}>
-          <KeyValue label={<FormattedMessage id="ui-orders.orderDetails.manualPO" />}>
-            <Checkbox checked={get(order, 'manualPo')} disabled />
-          </KeyValue>
-        </Col>
-        <Col xs={6}>
-          <KeyValue label={<FormattedMessage id="ui-orders.orderDetails.re_encumber" />}>
-            <Checkbox checked={get(order, 'reEncumber')} disabled />
-          </KeyValue>
-        </Col>
-        <Col xs={6}>
-          <KeyValue
-            label={<FormattedMessage id="ui-orders.orderDetails.orderType" />}
-            value={get(order, 'orderType')}
-          />
-        </Col>
-        <Col
-          data-test-order-details-owner
-          xs={6}
-        >
-          <KeyValue
-            label={<FormattedMessage id="ui-orders.poLine.owner" />}
-            value={get(order, 'owner')}
-          />
-        </Col>
-        <Col xs={12}>
-          {get(order, 'notes', []).map((note, index) => (
-            <KeyValue
-              key={index}
-              label={<FormattedMessage id="ui-orders.orderDetails.note" />}
-              value={note}
+      <Fragment>
+        <Row>
+          <Col xs={12}>
+            <MetaSection
+              id="polItemMeta"
+              lastUpdatedBy={get(metadata, 'updatedByUserId')}
+              lastUpdatedDate={get(metadata, 'updatedDate')}
+              createdBy={get(metadata, 'createdByUserId')}
+              createdDate={get(metadata, 'createdDate')}
             />
-          ))}
-        </Col>
-      </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={6}>
+            <KeyValue
+              label={<FormattedMessage id="ui-orders.orderDetails.vendor" />}
+              value={get(order, 'vendorName')}
+            />
+          </Col>
+          <Col xs={6}>
+            <KeyValue
+              label={<FormattedMessage id="ui-orders.orderDetails.createdBy" />}
+              value={get(order, 'createdByName')}
+            />
+          </Col>
+          <Col xs={6}>
+            <KeyValue
+              label={<FormattedMessage id="ui-orders.orderDetails.poNumber" />}
+              value={get(order, 'poNumber')}
+            />
+          </Col>
+          <Col xs={6}>
+            <KeyValue label={<FormattedMessage id="ui-orders.orderDetails.createdOn" />}>
+              <FolioFormattedTime dateString={get(order, 'metadata.createdDate')} />
+            </KeyValue>
+          </Col>
+          <Col xs={6}>
+            <KeyValue
+              label={<FormattedMessage id="ui-orders.orderDetails.assignedTo" />}
+              value={get(order, 'assignedToUser')}
+            />
+          </Col>
+          <Col
+            className={css.addressWrapper}
+            data-test-order-details-bill-to
+            xs={6}
+          >
+            <KeyValue
+              label={<FormattedMessage id="ui-orders.orderDetails.billTo" />}
+              value={addressBillTo}
+            />
+          </Col>
+          <Col
+            className={css.addressWrapper}
+            data-test-order-details-ship-to
+            xs={6}
+          >
+            <KeyValue
+              label={<FormattedMessage id="ui-orders.orderDetails.shipTo" />}
+              value={addressShipTo}
+            />
+          </Col>
+          <Col xs={6}>
+            <KeyValue label={<FormattedMessage id="ui-orders.orderDetails.manualPO" />}>
+              <Checkbox checked={get(order, 'manualPo')} disabled />
+            </KeyValue>
+          </Col>
+          <Col xs={6}>
+            <KeyValue label={<FormattedMessage id="ui-orders.orderDetails.re_encumber" />}>
+              <Checkbox checked={get(order, 'reEncumber')} disabled />
+            </KeyValue>
+          </Col>
+          <Col xs={6}>
+            <KeyValue
+              label={<FormattedMessage id="ui-orders.orderDetails.orderType" />}
+              value={get(order, 'orderType')}
+            />
+          </Col>
+          <Col
+            data-test-order-details-owner
+            xs={6}
+          >
+            <KeyValue
+              label={<FormattedMessage id="ui-orders.poLine.owner" />}
+              value={get(order, 'owner')}
+            />
+          </Col>
+          <Col xs={12}>
+            {get(order, 'notes', []).map((note, index) => (
+              <KeyValue
+                key={index}
+                label={<FormattedMessage id="ui-orders.orderDetails.note" />}
+                value={note}
+              />
+            ))}
+          </Col>
+        </Row>
+      </Fragment>
     );
   }
 }
