77769b3c6bbfc81a332d661714204874845174c9
Yury Aniskou <klaus24128@gmail.com>
2019-06-26 13:46:04 +0300

    UIOR-277 fix for eslint

--- src/common/POFields/FieldVendor.js
+++ src/common/POFields/FieldVendor.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import FieldSelection from '../FieldSelection';
-import { Required } from "../../components/Utils/Validate";
+import { Required } from '../../components/Utils/Validate';
 
 const FieldVendor = ({ vendors }) => {
   return (
