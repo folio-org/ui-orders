e43fee4f7b3687bdeafd5e183256811c87d013cc
Yury Aniskou <klaus24128@gmail.com>
2019-09-25 14:38:37 +0300

    UIOR-383 add transition to edit from POL details view

--- src/components/POLine/POLineView.js
+++ src/components/POLine/POLineView.js
@@ -43,6 +43,7 @@ import {
   PHRESOURCES,
   OTHER,
 } from './const';
+import { FILTERS } from '../../OrdersList';
 
 class POLineView extends Component {
   static propTypes = {
@@ -110,10 +111,14 @@ class POLineView extends Component {
 
   onEditPOLine = (e) => {
     if (e) e.preventDefault();
-    const { queryMutator } = this.props;
+    const { queryMutator, order, line } = this.props;
 
-    queryMutator.update({ layer: 'edit-po-line' });
-  }
+    queryMutator.update({
+      _path: `/orders/view/${order.id}/po-line/view/${line.id}`,
+      layer: 'edit-po-line',
+      filters: `${FILTERS.STATUS}.${WORKFLOW_STATUS.open},${FILTERS.STATUS}.${WORKFLOW_STATUS.pending}`,
+    });
+  };
 
   mountDeleteLineConfirm = () => this.setState({ showConfirmDelete: true });
 
