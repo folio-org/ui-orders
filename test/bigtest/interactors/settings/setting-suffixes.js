40ae4f24b0f13ee5feca79aa00c26cd5f677dc36
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2019-10-11 14:55:06 +0300

    fix tests

--- test/bigtest/interactors/settings/setting-suffixes.js
+++ test/bigtest/interactors/settings/setting-suffixes.js
@@ -1,7 +1,7 @@
 import {
   interactor,
 } from '@bigtest/interactor';
-import MultiSelectionInteractor from '@folio/stripes-acq-components/test/bigtest/interactor';
+import MultiSelectionInteractor from '@folio/stripes-acq-components/test/bigtest/interactors';
 
 export default interactor(class SettingSuffixesInteractor {
   static defaultScope = '[data-test-order-settings-suffixes]';
