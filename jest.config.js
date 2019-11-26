b88cc29cbb72d83b1dead632e318f15d7b280fbc
Mikita Siadykh <mikita_siadykh@epam.com>
2019-11-26 13:28:30 +0300

    coverage

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 const esModules = ['@folio'].join('|');
 
 module.exports = {
--- src/common/UserFilter/UserFilter.test.jsx
+++ src/common/UserFilter/UserFilter.test.jsx
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import { IntlProvider } from 'react-intl';
