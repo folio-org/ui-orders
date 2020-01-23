c6fe09623e5e4fe6070ef865792e181314d3ed25
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2020-01-23 12:35:00 +0300

    fix tests 2

--- test/bigtest/interactors/line-details-page.js
+++ test/bigtest/interactors/line-details-page.js
@@ -40,17 +40,17 @@ export default interactor(class LineDetailsPage {
   checkInButton = new Button('[data-test-line-check-in-button]');
   otherDetailsAccordion = isPresent('#other');
   goBackToOrderButton = new Button('#clickable-backToPO');
-  isLoaded = isPresent('[class*=paneTitleLabel---]');
+  isLoaded = isPresent('#ItemDetails');
   relatedInvoicesAccordion = new RelatedInvoicesAccordion();
   notesAccordion = new NotesAccordion();
   lineInvoicesIsPresent = isPresent('#lineInvoices');
 
   actions = new LineDetailsPageActions();
   whenLoaded() {
-    return this.timeout(20000).when(() => this.isLoaded);
+    return this.timeout(TIMEOUT).when(() => this.isLoaded);
   }
 
   whenInvoicesLoaded() {
-    return this.timeout(20000).when(() => this.lineInvoicesIsPresent);
+    return this.timeout(TIMEOUT).when(() => this.lineInvoicesIsPresent);
   }
 });
