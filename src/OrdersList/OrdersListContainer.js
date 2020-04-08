b85b19d2c054267f056ef0244dc1c9b0885e9186
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2020-04-08 21:45:11 +0300

    remove code smells

--- src/OrdersList/OrdersListContainer.js
+++ src/OrdersList/OrdersListContainer.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import ReactRouterPropTypes from 'react-router-prop-types';
