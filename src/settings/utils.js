68e0c7253847a9976c222011f3bb3bc7879a3c76
Andrei Shumski2 <Andrei_Shumski2@epam.com>
2020-07-23 12:22:41 +0300

    add length validation

--- src/settings/utils.js
+++ src/settings/utils.js
@@ -4,7 +4,7 @@ import { FormattedMessage } from 'react-intl';
 const NAME_REGEXP = new RegExp(/[^a-zA-Z\d]/);
 
 export const validatePrefixSuffixName = ({ name }) => ({
-  name: name?.match(NAME_REGEXP)
+  name: name?.match(NAME_REGEXP) || name?.length > 7
     ? <FormattedMessage id="ui-orders.settings.poNumber.nameValidation" />
     : undefined,
 });
