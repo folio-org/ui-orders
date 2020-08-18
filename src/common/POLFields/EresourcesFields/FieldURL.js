f885def2fa00ceaf6e176533483fa03ac57cb3aa
Andrei Shumski2 <Andrei_Shumski2@epam.com>
2020-08-18 16:11:09 +0300

    f

--- src/common/POLFields/EresourcesFields/FieldURL.js
+++ src/common/POLFields/EresourcesFields/FieldURL.js
@@ -1,33 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { Field } from 'react-final-form';
 
 import { TextField } from '@folio/stripes/components';
 import { validateURL } from '@folio/stripes-acq-components';
 
-const FieldURL = ({ disabled, required }) => {
-  return (
-    <Field
-      component={TextField}
-      disabled={disabled}
-      label={<FormattedMessage id="ui-orders.eresource.url" />}
-      name="eresource.resourceUrl"
-      required={required}
-      type="text"
-      validate={validateURL}
-    />
-  );
-};
-
-FieldURL.propTypes = {
-  disabled: PropTypes.bool,
-  required: PropTypes.bool,
-};
-
-FieldURL.defaultProps = {
-  disabled: false,
-  required: false,
-};
+const FieldURL = props => (
+  <Field
+    component={TextField}
+    label={<FormattedMessage id="ui-orders.eresource.url" />}
+    name="eresource.resourceUrl"
+    type="text"
+    validate={validateURL}
+    {...props}
+  />
+);
 
 export default FieldURL;
