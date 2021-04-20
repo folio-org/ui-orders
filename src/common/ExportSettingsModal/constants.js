81924b6784bde90ac113bfec0a192625e74c3950
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2021-04-20 16:37:08 +0300

    UIOR-672 fix confirmation modal

--- src/common/ExportSettingsModal/constants.js
+++ src/common/ExportSettingsModal/constants.js
@@ -90,3 +90,16 @@ export const EXPORT_LINE_FIELDS_OPTIONS = Object.keys(EXPORT_LINE_FIELDS).map(fi
   label: EXPORT_LINE_FIELDS[field],
   value: field,
 }));
+
+export const PRINT_ORDER_FIELDS = {};
+export const PRINT_LINE_FIELDS = {};
+
+export const PRINT_ORDER_FIELDS_OPTIONS = Object.keys(PRINT_ORDER_FIELDS).map(field => ({
+  label: PRINT_ORDER_FIELDS[field],
+  value: field,
+}));
+
+export const PRINT_LINE_FIELDS_OPTIONS = Object.keys(PRINT_LINE_FIELDS).map(field => ({
+  label: PRINT_LINE_FIELDS[field],
+  value: field,
+}));
