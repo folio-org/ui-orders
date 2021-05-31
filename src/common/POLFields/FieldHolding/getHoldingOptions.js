a0aba365330267c03dbb8c91d78748f416389d24
Andrei Shumski <Andrei_Shumski2@epam.com>
2021-05-31 14:41:03 +0300

    code smell

--- src/common/POLFields/FieldHolding/getHoldingOptions.js
+++ src/common/POLFields/FieldHolding/getHoldingOptions.js
@@ -1,6 +1,6 @@
 import { getHoldingLocationName } from '../../utils';
 
-export const getHoldingOptions = (holdings = [], locationsMap) => (
+export const getHoldingOptions = (holdings = [], locationsMap = {}) => (
   holdings.map(holding => ({
     value: holding.id,
     label: getHoldingLocationName(holding, locationsMap),
