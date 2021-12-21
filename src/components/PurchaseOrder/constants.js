56913561571aeea91eebb92f2f8e0b84e0bf675f
Mikita Siadykh <mikita_siadykh@epam.com>
2021-12-21 13:24:55 +0400

    UIOR-868 Add estimated price to POL grid view on order record

--- src/components/PurchaseOrder/constants.js
+++ src/components/PurchaseOrder/constants.js
@@ -9,6 +9,7 @@ export const LINE_LISTING_COLUMN_MAPPING = {
   poLineNumber: <FormattedMessage id="ui-orders.poLine.number" />,
   title: <FormattedMessage id="ui-orders.lineListing.titleOrPackage" />,
   productId: <FormattedMessage id="ui-orders.lineListing.productId" />,
+  estimatedPrice: <FormattedMessage id="ui-orders.cost.estimatedPrice" />,
   vendorRefNumber: <FormattedMessage id="ui-orders.lineListing.refNumber" />,
   fundCode: <FormattedMessage id="ui-orders.lineListing.fundCode" />,
   arrow: null,
