ab9dd338406e8a45b1455cd28905328f77cd2444
Alisher Musurmonov <alisher_musurmonov1@epam.com>
2023-09-14 13:36:07 +0500

    tests: add test coverages

--- src/components/POLine/asyncValidate.test.js
+++ src/components/POLine/asyncValidate.test.js
@@ -10,12 +10,11 @@ const values = {
   productIdType: 'identifierTypeId',
 };
 const dispatch = jest.fn();
+const validationMock = jest.fn();
 const props = {
   parentMutator: {
     validateISBN: {
-      GET: jest.fn().mockResolvedValue({
-        isValid: true,
-      }),
+      GET: validationMock,
     },
   },
   parentResources: {
@@ -31,8 +30,25 @@ const blurredField = 'productIdType';
 
 describe('asyncValidate', () => {
   it('should not return validation errors if data is valid', async () => {
+    validationMock.mockResolvedValue({
+      isValid: true,
+    });
     const error = await asyncValidate(values, dispatch, props, blurredField);
 
     expect(error).toBeUndefined();
+  });
+
+  it('should return validation errors if ISBN is invalid', async () => {
+    let thrownError;
+
+    validationMock.mockResolvedValue({ isValid: false });
+
+    try {
+      thrownError = await asyncValidate(values, dispatch, props, blurredField);
+    } catch (error) {
+      thrownError = error;
+    }
+
+    expect(JSON.stringify(thrownError)).toEqual(expect.stringContaining('ui-orders.errors.invalidProductId'));
   });
 });
