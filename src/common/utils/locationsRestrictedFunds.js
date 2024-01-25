9fabf8de2780bc385cdedb171cb9e30083c951b3
Yury Saukou <yury_saukou@epam.com>
2024-01-25 20:59:25 +0400

    add js docs for filter functions

--- src/common/utils/locationsRestrictedFunds.js
+++ src/common/utils/locationsRestrictedFunds.js
@@ -1,8 +1,11 @@
 import intersection from 'lodash/intersection';
 import partition from 'lodash/partition';
 
-/*
+/**
   Return a list of funds that are not restricted by locations or restricted by specified locations.
+
+  @param {string[]} locationIds - A list of location IDs and holding's permanent location IDs assigned to a PO Line.
+  @param {object[]} funds - A list of funds to filter.
 */
 export const filterFundsRestrictedByLocations = (locationIds, funds) => {
   if (!locationIds?.length) return funds;
@@ -14,9 +17,13 @@ export const filterFundsRestrictedByLocations = (locationIds, funds) => {
   return filteredFunds;
 };
 
-/*
+/**
   Returns a list of locations that restrict funds. If at least one fund is not location-restricted, returns all available locations.
   Besides, it allows to take into account the initial value.
+
+  @param {object[]} funds - A list of funds assigned to a PO Line.
+  @param {object[]} locations - A list of locations to filter.
+  @param {string[]} includeLocationIds - A list of location IDs that should be persisted (Basically, used to keep initial value of the field).
 */
 export const filterLocationsByRestrictedFunds = (funds, locations, includeLocationIds = []) => {
   const [restrictedFunds, unrestrictedFunds] = partition(funds, ({ restrictByLocations }) => restrictByLocations);
@@ -33,9 +40,13 @@ export const filterLocationsByRestrictedFunds = (funds, locations, includeLocati
   return validLocations;
 };
 
-/*
+/**
   Returns a list of holdings for which their permanent locations restrict funds. If at least one fund is not location-restricted, returns all available holdings.
   Besides, it allows to take into account the initial value.
+
+  @param {object[]} funds - A list of funds assigned to a PO Line.
+  @param {object[]} holdings - A list of holdings to filter.
+  @param {string[]} includeHoldingIds - A list of holding IDs that should be persisted (Basically, used to keep initial value of the field).
 */
 export const filterHoldingsByRestrictedFunds = (funds, holdings, includeHoldingIds = []) => {
   const [restrictedFunds, unrestrictedFunds] = partition(funds, ({ restrictByLocations }) => restrictByLocations);
