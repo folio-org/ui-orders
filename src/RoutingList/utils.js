7783c040e47ee95d6525f40f66623ecb49d6c4aa
Alisher Musurmonov <alisher_musurmonov1@epam.com>
2024-05-02 20:33:20 +0500

    test: fix failing tests

--- src/RoutingList/utils.js
+++ src/RoutingList/utils.js
@@ -1,6 +1,6 @@
 import { FormattedMessage } from 'react-intl';
 
-export const validateRoutingListForm = ({ name }) => {
+export const validateRoutingListForm = ({ name = '' }) => {
   if (!name.trim().length) {
     return {
       name: <FormattedMessage id="ui-orders.routing.list.name.validation.required" />,
