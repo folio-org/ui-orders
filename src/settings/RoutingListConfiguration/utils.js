1190cdc3a6bc69527895ba60f9e0481a35d863be
Alisher Musurmonov <alisher_musurmonov1@epam.com>
2024-05-03 17:13:30 +0500

    test: improve test coverage

--- src/settings/RoutingListConfiguration/utils.js
+++ src/settings/RoutingListConfiguration/utils.js
@@ -1,13 +1,11 @@
 import { get } from 'lodash';
 import { FormattedMessage } from 'react-intl';
 
-const ESCAPE_HTML_TAGS_REGEX = /<(.|\n)*?>/g;
-
 export const validateRoutingListConfigurationForm = (values) => {
   const { localizedTemplates } = values;
   const body = get(localizedTemplates, 'en.body', '');
 
-  if (!body.replace(ESCAPE_HTML_TAGS_REGEX, '').trim().length) {
+  if (!body.replace(/<(.|\n)*?>/g, '').trim().length) {
     return {
       localizedTemplates: {
         en: {
