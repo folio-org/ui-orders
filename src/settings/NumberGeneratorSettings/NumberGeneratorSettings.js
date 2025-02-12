d66c24100e0b702fb95490f1c80b4a909b15927e
elsenhans <elsenhans@users.noreply.github.com>
2025-02-12 14:57:09 +0100

    UIOR-1383-settings-for-number-generator add NumberGeneratorSettingsForm

---
+++ src/settings/NumberGeneratorSettings/NumberGeneratorSettings.js
@@ -0,0 +1,9 @@
+import NumberGeneratorSettingsForm from './NumberGeneratorSettingsForm';
+
+const onSubmit = async () => {};
+
+function NumberGeneratorSettings() {
+  return (<NumberGeneratorSettingsForm initialValues={{}} onSubmit={onSubmit} />);
+}
+
+export default NumberGeneratorSettings;
---
+++ src/settings/NumberGeneratorSettings/NumberGeneratorSettingsForm.js
@@ -0,0 +1,161 @@
+import PropTypes from 'prop-types';
+import { Field } from 'react-final-form';
+import { FormattedMessage, useIntl } from 'react-intl';
+
+import {
+  Accordion,
+  AccordionSet,
+  Button,
+  Checkbox,
+  Col,
+  MessageBanner,
+  Pane,
+  PaneFooter,
+  PaneHeader,
+  Row,
+  Select,
+} from '@folio/stripes/components';
+import stripesFinalForm from '@folio/stripes/final-form';
+
+import css from './NumberGeneratorSettings.css';
+import {
+  ACCESSION_NUMBER_SETTING,
+  BARCODE_SETTING,
+  CALL_NUMBER_SETTING,
+  NUMBER_GENERATOR_OPTIONS,
+  NUMBER_GENERATOR_OPTIONS_OFF,
+  USE_SHARED_NUMBER,
+} from './constants';
+
+const NumberGeneratorSettingsForm = ({
+  handleSubmit,
+  pristine,
+  submitting,
+  values,
+}) => {
+  const intl = useIntl();
+
+  const disableSharedNumber =
+    values?.accessionNumber === NUMBER_GENERATOR_OPTIONS_OFF ||
+    values?.callNumber === NUMBER_GENERATOR_OPTIONS_OFF;
+  const disableGeneratorOffOption = values?.useSharedNumber;
+
+  const getTranslatedDataOptions = (field, shouldDisableOff) => {
+    return field.map(item => ({
+      label: item.value ? intl.formatMessage({ id: `ui-orders.settings.numberGenerator.options.${item.value}` }) : '',
+      value: item.value,
+      disabled: shouldDisableOff && item.value === NUMBER_GENERATOR_OPTIONS_OFF,
+    }));
+  };
+
+  const dataOptionsAllEnabled = getTranslatedDataOptions(NUMBER_GENERATOR_OPTIONS, false);
+  const dataOptionsOffDisabled = getTranslatedDataOptions(NUMBER_GENERATOR_OPTIONS, disableGeneratorOffOption);
+
+  const paneHeader = (renderProps) => (
+    <PaneHeader
+      {...renderProps}
+      paneTitle={<FormattedMessage id="ui-orders.settings.numberGenerator.options" />}
+    />
+  );
+
+  const paneFooter = (
+    <PaneFooter
+      renderEnd={
+        <Button
+          buttonStyle="primary mega"
+          disabled={pristine || submitting}
+          id="clickable-save-number-generator-settings"
+          onClick={handleSubmit}
+          type="submit"
+        >
+          <FormattedMessage id="stripes-core.button.save" />
+        </Button>
+      }
+    />
+  );
+
+  return (
+    <form onSubmit={handleSubmit} id="numberGeneratorSettingsForm">
+      <Pane defaultWidth="fill" footer={paneFooter} id="number-generator-settings-form" renderHeader={paneHeader}>
+        <Row className={css.marginBottomGutterDouble}>
+          <Col xs={12}>
+            <MessageBanner>
+              <p><FormattedMessage id="ui-orders.settings.numberGenerator.info" /></p>
+            </MessageBanner>
+          </Col>
+        </Row>
+        <AccordionSet>
+          <Accordion
+            id="acc01"
+            label={<FormattedMessage id="ui-orders.settings.numberGenerator.receiving" />}
+          >
+            <Row className={css.marginBottomGutterDouble}>
+              <Col xs={6}>
+                <Field
+                  component={Select}
+                  id={BARCODE_SETTING}
+                  label={<FormattedMessage id="ui-orders.settings.numberGenerator.barcode" />}
+                  name={BARCODE_SETTING}
+                  dataOptions={dataOptionsAllEnabled}
+                />
+              </Col>
+            </Row>
+            <Row className={css.marginBottomGutterDouble}>
+              <Col xs={6}>
+                <Field
+                  component={Select}
+                  id={ACCESSION_NUMBER_SETTING}
+                  label={<FormattedMessage id="ui-orders.settings.numberGenerator.accessionNumber" />}
+                  name={ACCESSION_NUMBER_SETTING}
+                  dataOptions={dataOptionsOffDisabled}
+                />
+              </Col>
+            </Row>
+            <Row className={css.marginBottomGutterDouble}>
+              <Col xs={6}>
+                <Field
+                  component={Select}
+                  id={CALL_NUMBER_SETTING}
+                  label={<FormattedMessage id="ui-orders.settings.numberGenerator.callNumber" />}
+                  name={CALL_NUMBER_SETTING}
+                  dataOptions={dataOptionsOffDisabled}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12}>
+                <Field
+                  component={Checkbox}
+                  disabled={disableSharedNumber}
+                  id={USE_SHARED_NUMBER}
+                  label={<FormattedMessage id="ui-orders.settings.numberGenerator.accessionNumberEqualCallNumber" />}
+                  name={USE_SHARED_NUMBER}
+                  type="checkbox"
+                />
+                {disableSharedNumber &&
+                  <MessageBanner type="warning">
+                    <FormattedMessage id="ui-orders.settings.numberGenerator.accessionNumberEqualCallNumber.warning" />
+                  </MessageBanner>
+                }
+              </Col>
+            </Row>
+          </Accordion>
+        </AccordionSet>
+      </Pane>
+    </form>
+  );
+};
+
+NumberGeneratorSettingsForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  pristine: PropTypes.bool.isRequired,
+  submitting: PropTypes.bool.isRequired,
+  values: PropTypes.object,
+};
+
+export default stripesFinalForm({
+  enableReinitialize: true,
+  keepDirtyOnReinitialize: true,
+  navigationCheck: true,
+  subscription: { values: true },
+})(NumberGeneratorSettingsForm);
