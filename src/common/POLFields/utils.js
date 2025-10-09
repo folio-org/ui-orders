568d78df67b59208c3c3ec62bb84ec078502fb18
Yury Saukou <yury_saukou@epam.com>
2025-10-09 13:42:32 +0400

    UIOR-1476 Disable 'Bindery active' checkbox on open order when criteria are not met

--- src/common/POLFields/utils.js
+++ src/common/POLFields/utils.js
@@ -1,9 +1,14 @@
+import get from 'lodash/get';
+
 import { ORDER_FORMATS } from '@folio/stripes-acq-components';
 
 import {
   ERESOURCES,
+  OPTION_VALUE_WITH_BINDERY_ACTIVE,
   PHRESOURCES,
 } from '../../components/POLine/const';
+import { isWorkflowStatusNotPending } from '../../components/PurchaseOrder/util';
+import { POL_FORM_FIELDS } from '../constants';
 
 export const isEresource = (format) => ERESOURCES.includes(format);
 
@@ -11,9 +16,20 @@ export const isPhresource = (format) => PHRESOURCES.includes(format);
 
 export const isOtherResource = (format) => format === ORDER_FORMATS.other;
 
-export const isBinderyActiveDisabled = (orderFormat) => {
+export const isBinderyActiveDisabled = (values, order) => {
+  const orderFormat = get(values, POL_FORM_FIELDS.orderFormat);
+  const physicalCreateInventory = get(values, POL_FORM_FIELDS.physicalCreateInventory);
+
   if (!orderFormat) {
     return false;
+  }
+
+  if (
+    order
+    && isWorkflowStatusNotPending(order)
+    && physicalCreateInventory !== OPTION_VALUE_WITH_BINDERY_ACTIVE
+  ) {
+    return true;
   }
 
   return (
