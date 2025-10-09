7dcc3c735990b63d30bcd25cf3c8195e3851a02a
Yury Saukou <yury_saukou@epam.com>
2025-10-09 15:47:23 +0400

    update logic

--- src/common/POLFields/utils.js
+++ src/common/POLFields/utils.js
@@ -20,20 +20,20 @@ export const isBinderyActiveDisabled = (values, order) => {
   const orderFormat = get(values, POL_FORM_FIELDS.orderFormat);
   const physicalCreateInventory = get(values, POL_FORM_FIELDS.physicalCreateInventory);
 
-  if (!orderFormat) {
-    return false;
-  }
+  if (!orderFormat) return false;
 
-  if (
+  /*
+    Check if the order format is NOT 'Physical Resource' or 'Physical/Electronic Mix'
+  */
+  if (![ORDER_FORMATS.physicalResource, ORDER_FORMATS.PEMix].includes(orderFormat)) return true;
+
+  /*
+    Disable the field if the order is not in pending status (Opened, Closed) and
+    the `physical.createInventory` field does not have the value 'Instance, Holding, Item'
+  */
+  return (
     order
     && isWorkflowStatusNotPending(order)
     && physicalCreateInventory !== OPTION_VALUE_WITH_BINDERY_ACTIVE
-  ) {
-    return true;
-  }
-
-  return (
-    orderFormat !== ORDER_FORMATS.PEMix
-    && orderFormat !== ORDER_FORMATS.physicalResource
   );
 };
