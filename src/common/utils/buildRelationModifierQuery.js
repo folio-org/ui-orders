f9afd49b077eca2f7179db40d3b88057c348471f
Yury Saukou <yury_saukou@epam.com>
2026-06-24 16:17:49 +0400

    update util name

--- src/common/utils/buildRelationModifierQuery.js
+++ src/common/utils/buildRelationModifierQuery.js
@@ -1,7 +1,7 @@
-import { buildMultiSelectionCqlQuery } from '@folio/stripes-acq-components';
+import { buildMultiOptionCqlQuery } from '@folio/stripes-acq-components';
 
 export const buildRelationModifierQuery = (filterKey, relationModifier, filterValue) => {
   const modifiers = [{ name: relationModifier }];
 
-  return buildMultiSelectionCqlQuery(filterKey, filterValue, { modifiers });
+  return buildMultiOptionCqlQuery(filterKey, filterValue, { modifiers });
 };
