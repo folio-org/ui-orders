da6a17ba407ce2595104c675c3eb762636d35e0d
Mikita Siadykh <mikita_siadykh@epam.com>
2026-09-01 10:50:29 +0300

    UIOR-1552 fix inactiveExpenseClass message

--- src/components/Utils/order/handleErrorsStrategies/inactiveExpenseClassStrategy.js
+++ src/components/Utils/order/handleErrorsStrategies/inactiveExpenseClassStrategy.js
@@ -1,13 +1,12 @@
 export const inactiveExpenseClassStrategy = ({ callout }) => {
   const handle = (errorsContainer) => {
     const error = errorsContainer.getError();
-    const fundCode = error.getParameter('fundCode');
     const expenseClassName = error.getParameter('expenseClassName');
 
     callout.sendCallout({
       messageId: `ui-orders.errors.openOrder.${error.code}`,
       type: 'error',
-      values: { fundCode, expenseClassName },
+      values: { expenseClass: expenseClassName },
     });
   };
 
